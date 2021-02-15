757c4a4b491e10b2ace1ce638992f9ae94798878
Andrew Hill <andrew.hill@random.studio>
2021-02-15 10:30:57 +0100

    Fix email

--- components/Actions/Action/Action.js
+++ components/Actions/Action/Action.js
@@ -4,7 +4,7 @@ const Action = ({ copy, isContact, title }) => {
 	const Component = isContact ? 'a' : 'div';
 	const classNames = `${styles.tile} ${title ? styles.hasTitle : ''} ${isContact ? styles.isContact : ''}`;
 	return (
-		<Component className={classNames} {...(isContact ? { href: 'mailto:rachael.short@randomstudio' } : {})}>
+		<Component className={classNames} {...(isContact ? { href: 'mailto:sutainability@random.studio' } : {})}>
 			{title && <h3 className={styles.title}>{title}</h3>}
 			<p className={styles.copy}>{copy}</p>
 		</Component>
