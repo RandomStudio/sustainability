4f3729e9129cabb30562064fd8dcc03e96e30da0
Andrew Hill <andrew.hill@random.studio>
2021-02-15 11:44:53 +0100

    Reset menu on any page navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,25 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const [isChecked, setIsChecked] = useState(false);
+  const router = useRouter();
+  
+  useEffect(() => {
+    setIsChecked(false);
+  }, [router]);
+
   return (
     <>
       <Head>
         <title>Sustainability Toolkit</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-			<input id="navcheckbox" type="checkbox" />
+			<input checked={isChecked} id="navcheckbox" onChange={({ target: { checked } }) => setIsChecked(checked)} type="checkbox" />
       <Header />
       <main className="main">
         <Component {...pageProps} />
