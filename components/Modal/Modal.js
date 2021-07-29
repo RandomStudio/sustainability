677167a8afa03bdb8ac0b09f726be803dac8bebd
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2021-07-29 15:14:09 +0200

    remove artifact from multiple classes

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -3,7 +3,7 @@ import { forwardRef } from "react";
 
 const Modal = forwardRef(({ children, setIsOpen }, ref) => {
   return (
-    <div ref={ref} className={`${styles.modal}`}>
+    <div ref={ref} className={styles.modal}>
       <div className={styles.overlay} onClick={() => setIsOpen(false)} />
       <div className={styles.wrapper}>
         <div className={styles.content}>{children}</div>
